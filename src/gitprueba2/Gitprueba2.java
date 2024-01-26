efe45201d39b4e8f3cc7425066309bbdeb544799
Jose R Seron <joserobertoseron@gmail.com>
2024-01-25 20:26:42 -0600

    commit 2

--- src/gitprueba2/Gitprueba2.java
+++ src/gitprueba2/Gitprueba2.java
@@ -4,6 +4,8 @@
  */
 package gitprueba2;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author joser
@@ -14,7 +16,13 @@ public class Gitprueba2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here}
+        System.out.println("fokin git"
+                + "ad");
+        
+        JOptionPane.showMessageDialog(null, "este es el segundo commit");
+        
+        
     }
     
 }
